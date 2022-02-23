89a75299697e6b21ad6d825716b71d6a7fbecc7c
yujongyeob <230233@naver.com>
2022-02-23 17:05:28 +0900

    views관련 url 편집

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
+    path('',base_views.index, name='index'),
 
 ]
